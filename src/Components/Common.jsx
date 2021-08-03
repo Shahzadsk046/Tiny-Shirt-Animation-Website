bd54f661a8a795c6c69e5176425ba12ff235cb97
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2021-08-04 00:43:35 +0500

    Updated App.css

--- src/Components/Common.jsx
+++ src/Components/Common.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import useWebAnimations, { fadeInLeft} from "@wellyshen/use-web-animations";
+import React, {useRef} from "react";
 import { NavLink } from "react-router-dom";
 
 // import c1 from '../images/c1.gif';
 
 const Common = (props) => {
+
+
+  // const {keyframes, animationOptions} = fadeInLeft;
+  // const {ref} = useWebAnimations({
+    // ...h1ref,
+    // ...h2ref,
+    // ...btnref,
+    // keyframes:[
+    //   ...keyframes,
+    // ],
+    // animationOptions:{
+    //   ...animationOptions,
+      // duration: animationOptions.duration * 6,
+      // easing: 'ease-in-out',
+    // },
+    // keyframes:[
+    //   // {transform: "translateX(500px)"},
+    //   {transform: "translate(0,0)"},
+    //   {transform: "translate(500px,0)"},
+    //   // {transform: "translateX(500px)"},
+    // ],
+    // animationOptions:{
+    //   duration:1000,
+    //   // iterations: Infinity,
+    //   // direction: "up-down",
+    //   // easing: "ease-in-out",
+    // }
+    // ...bounce
+  //   ...fadeInLeft,
+  // })
+  const h1ref = useWebAnimations({...fadeInLeft})
+  const h2ref =  useWebAnimations({
+    ...fadeInLeft,
+  animationOptions: {
+    delay:500,
+    duration: 1000,
+    fill: "forwards",
+    
+    
+  }    
+  })
+  const btnref =  useWebAnimations({...fadeInLeft,
+    animationOptions: {
+      delay:1000,
+      duration: 1000,}})
+
   return (
       <section id="header" className="d-flex align-items-center">
         <div className="container-fluid">
@@ -11,14 +58,14 @@ const Common = (props) => {
             <div className="col-10 mx-auto">
               <div className="row">
                 <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                  <h1>
+                  <h1 ref={h1ref.ref}>
                     {props.name}
                     <strong className="brand-name">SK Tutorials</strong>
                   </h1>
-                  <h2 className="my-3">
+                  <h2 className="my-3" ref={h2ref.ref}>
                     We are the of talented developer making websites
                   </h2>
-                  <div className="mt-3">
+                  <div className="mt-3" ref={btnref.ref}>
                     <NavLink to={props.visit} className="btn-get-started">
                       {props.btname}
                     </NavLink>
