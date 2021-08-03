c6f1975852e61e2d6af30581791a6635ddaa5424
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2021-08-03 19:38:21 +0500

    Updated Cards with Sdata props

--- src/Components/Common.jsx
+++ src/Components/Common.jsx
@@ -5,7 +5,7 @@ import c1 from '../images/c1.gif';
 const Common = (props) => {
   return (
       <section id="header" className="d-flex align-items-center">
-        <div className="container-fluid nav_bg">
+        <div className="container-fluid">
           <div className="row">
             <div className="col-10 mx-auto">
               <div className="row">
---
+++ src/Components/Sdata.jsx
@@ -0,0 +1,35 @@
+import web from '../images/web.jpg';
+import android from '../images/android.png';
+import app from '../images/app.png';
+import digital from '../images/digital.jpg';
+import marketing from '../images/marketing.jpg';
+import software from '../images/software.png';
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "Web Development",
+    },
+    {
+        imgsrc: app,
+        title: "App Development",
+    },
+    {
+        imgsrc: software,
+        title: "Software Development",
+    },
+    {
+        imgsrc: digital,
+        title: "Digital Marketing",
+    },
+    {
+        imgsrc: android,
+        title: "Android Development",
+    },
+    {
+        imgsrc: marketing,
+        title: "Marketing",
+    },
+];
+
+export default Sdata;
